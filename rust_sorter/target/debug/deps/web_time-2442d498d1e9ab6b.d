561b05653e84b0eb179787fc573a0d94bb363f9e
Roberto Amaro <romaro1101@gmail.com>
2025-01-12 20:34:40 -0600

    Unknown metachanges

--- rust_sorter/target/debug/deps/web_time-2442d498d1e9ab6b.d
+++ rust_sorter/target/debug/deps/web_time-2442d498d1e9ab6b.d
@@ -1,5 +1,5 @@
-C:\Users\GPOSMPC19\OneDrive - Resistencias San Marino, S.A. de C.V\Documentos\Varios\Sorter\rust_sorter\target\debug\deps\libweb_time-2442d498d1e9ab6b.rmeta: C:\Users\GPOSMPC19\.cargo\registry\src\index.crates.io-6f17d22bba15001f\web-time-1.1.0\src\lib.rs
+D:\Docs\GIT REPOS\Sorter\rust_sorter\target\debug\deps\libweb_time-2442d498d1e9ab6b.rmeta: C:\Users\Ro\.cargo\registry\src\index.crates.io-6f17d22bba15001f\web-time-1.1.0\src\lib.rs
 
-C:\Users\GPOSMPC19\OneDrive - Resistencias San Marino, S.A. de C.V\Documentos\Varios\Sorter\rust_sorter\target\debug\deps\web_time-2442d498d1e9ab6b.d: C:\Users\GPOSMPC19\.cargo\registry\src\index.crates.io-6f17d22bba15001f\web-time-1.1.0\src\lib.rs
+D:\Docs\GIT REPOS\Sorter\rust_sorter\target\debug\deps\web_time-2442d498d1e9ab6b.d: C:\Users\Ro\.cargo\registry\src\index.crates.io-6f17d22bba15001f\web-time-1.1.0\src\lib.rs
 
-C:\Users\GPOSMPC19\.cargo\registry\src\index.crates.io-6f17d22bba15001f\web-time-1.1.0\src\lib.rs:
+C:\Users\Ro\.cargo\registry\src\index.crates.io-6f17d22bba15001f\web-time-1.1.0\src\lib.rs:
--- rust_sorter/target/debug/rust_sorter.d
+++ rust_sorter/target/debug/rust_sorter.d
@@ -1,1 +1,1 @@
-C:\Users\GPOSMPC19\OneDrive\ -\ Resistencias\ San\ Marino,\ S.A.\ de\ C.V\Documentos\Varios\Sorter\rust_sorter\target\debug\rust_sorter.exe: C:\Users\GPOSMPC19\OneDrive\ -\ Resistencias\ San\ Marino,\ S.A.\ de\ C.V\Documentos\Varios\Sorter\rust_sorter\src\main.rs
+D:\Docs\GIT\ REPOS\Sorter\rust_sorter\target\debug\rust_sorter.exe: D:\Docs\GIT\ REPOS\Sorter\rust_sorter\src\main.rs
